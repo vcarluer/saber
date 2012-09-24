9faeab881d3638a483a3d8e4b1bf923dc3d0750c
vcarluer <vcarluer@gmail.com>
2012-09-25 01:22:26 +0200

    navigation buttons

--- gen/gamer/associate/R.java
+++ gen/gamer/associate/R.java
@@ -14,6 +14,8 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
+        public static final int btBack=0x7f050001;
+        public static final int btSearch=0x7f050002;
         public static final int webViewMain=0x7f050000;
     }
     public static final class layout {
@@ -21,6 +23,8 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040001;
+        public static final int back=0x7f040002;
         public static final int hello=0x7f040000;
+        public static final int search=0x7f040003;
     }
 }
--- src/gamer/associate/SABActivity.java
+++ src/gamer/associate/SABActivity.java
@@ -2,14 +2,21 @@ package gamer.associate;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 import android.view.Window;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ImageView;
 import android.widget.Toast;
+import android.webkit.GeolocationPermissions;
 
 public class SABActivity extends Activity {
-    /** Called when the activity is first created. */
+    private static final String HOME_SEARCH = "http://www.google.com";
+	private WebView webview;
+	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,9 +25,32 @@ public class SABActivity extends Activity {
 
         setContentView(R.layout.main);
         
-        WebView webview = (WebView) this.findViewById(R.id.webViewMain);
+        this.webview = (WebView) this.findViewById(R.id.webViewMain);
+        
+        ImageView btBack = (ImageView) this.findViewById(R.id.btBack);
+        btBack.setOnTouchListener(new OnTouchListener() {
+			
+			public boolean onTouch(View v, MotionEvent event) {
+				if (webview.canGoBack()) {
+					webview.goBack();
+				}
+				
+				return true;
+			}
+		});
+        
+        ImageView btSearch = (ImageView) this.findViewById(R.id.btSearch);
+        btSearch.setOnTouchListener(new OnTouchListener() {
+			
+			public boolean onTouch(View v, MotionEvent event) {
+				webview.loadUrl(HOME_SEARCH);
+				return true;
+			}
+		});
+        
         
         webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setBuiltInZoomControls(true);
         
         final Activity activity = this;
         webview.setWebChromeClient(new WebChromeClient() {
@@ -29,13 +59,19 @@ public class SABActivity extends Activity {
         	     // The progress meter will automatically disappear when we reach 100%
         	     activity.setProgress(progress * 1000);
         	   }
+        	   
+//        	   public void onGeolocationPermissionsShowPrompt(String origin, GeolocationPermissions.Callback callback) {
+//        		    // callback.invoke(String origin, boolean allow, boolean remember);
+//        		    callback.invoke(origin, true, false);
+//        		  }
         	 });
-        	 webview.setWebViewClient(new WebViewClient() {
-        	   public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-        	     Toast.makeText(activity, "d'oh! " + description, Toast.LENGTH_SHORT).show();
-        	   }
-        	 });
+        
+    	 webview.setWebViewClient(new WebViewClient() {
+    	   public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+    	     Toast.makeText(activity, "d'oh! " + description, Toast.LENGTH_SHORT).show();
+    	   }
+    	 });
 
-        	 webview.loadUrl("http://www.google.com");
+    	 webview.loadUrl(HOME_SEARCH);
     }
 }
